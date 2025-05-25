6dfc309ff8081653e0f6699a93e71b40083c2c7d
MATHEUS DOS SANTOS NEVES <matheus.neves@southsystem.com.br>
2025-05-25 19:32:24 -0300

    feat: Add API and web feature tests for client, deliverer, stock, history, ingredient, user, order, and dish management
    - Implemented API tests for managing clients, deliverers, stock, history, ingredients, users, orders, and dishes.
    - Added web tests for managing clients, deliverers, stock, history, ingredients, users, orders, and dishes.
    - Included login feature tests for both API and web interfaces.
    - Created custom commands for API requests in Cypress.

--- cypress.config.js
+++ cypress.config.js
@@ -7,12 +7,62 @@ module.exports = defineConfig({
     url_api: "https://controlfoodapi-d8a49e8667a8.herokuapp.com/api/",
   },
   e2e: {
+    testIsolation: false,
+    numTestsKeptInMemory: 5,
+    userAgent: "None",
+    experimentalMemoryManagement: false,
+    experimentalRunAllSpecs: true,
+    viewportWidth: 1280, // Largura da viewport
+    viewportHeight: 768, // Altura da viewport
+    chromeWebSecurity: false, // Desativa a segurança do Chrome
+    defaultCommandTimeout: 60000, // Timeout padrão para comandos
+    responseTimeout: 60000, // Timeout para respostas
+    requestTimeout: 60000, // Timeout para requisições
+    trashAssetsBeforeRuns: false, // Não remove assets antes das execuções
+    watchForFileChanges: false, // Não observa mudanças nos arquivos
+    cacheAcrossSpecs: false, // Não mantém cache entre specs
+    video: false, // Desativa gravação de vídeo
     setupNodeEvents(on, config) {
-      on('file:preprocessor', cucumber())
+      on('file:preprocessor', cucumber());
 
     },
-    specPattern: "cypress/e2e/step_definitions/bdds/*.feature", // Padrão para localizar arquivos de teste
+    specPattern: "cypress/e2e/step_definitions/*.feature", // Padrão para localizar arquivos de teste
     baseUrl: "https://controlfoodweb-a3dbc52a8c88.herokuapp.com/", // URL base para os testes
   },
 });
 
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    // reporter: "mochawesome", // Configura o reporter
+    // reporterOptions: {
+    //   reportDir: "mochawesome-report", // Diretório do relatório
+    //   overwrite: true, // Sobrescreve relatórios existentes
+    //   html: true, // Gera relatório em HTML
+    //   json: true, // Gera relatório em JSON
+    //   timestamp: "mmddyyyy_hhmmss", // Formato do timestamp
+    // },
+    // specPattern: "*/.feature", // Padrão para localizar arquivos de teste
+    // baseUrl: urlFrontend, // URL base para os testes
+    // setupNodeEvents(on, config) {
+    //   on("before:browser:launch", (browser, launchOptions) => {
+    //     if (["chrome", "edge"].includes(browser.name)) {
+    //       if (browser.isHeadless) {
+    //         launchOptions.args.push("--no-sandbox");
+    //         launchOptions.args.push("--disable-gl-drawing-for-tests");
+    //         launchOptions.args.push("--disable-gpu");
+    //       }
+    //       launchOptions.args.push("--js-flags=--max-old-space-size=3500");
+    //     }
+    //     return launchOptions;
+    //  });
